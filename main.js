5dc52c4b0efb29b95357c1230c6b1a6ac6b9870d
chenxuanfeng <chenxuanfeng@meituan.com>
2020-01-08 17:51:47 +0800

    up

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-// 打 npm 包入口文件 
-
 import PCDatePicker from './src/datepicker/index.vue';
-export { DATE_TYPE } from './src/datepicker/lib/config'
+export { DATE_TYPE } from './src/datepicker/lib/config';
+export { dayCountByMonth, monthInfoByDate, getYearMonthNum, dateByDate, getTodayDate, getYesterday, getLastMonthDay, getRegionDay, dateByDateText, weekInfoByDate } from './src/datepicker/lib/tools-date';
 
 PCDatePicker.install = Vue => Vue.component(PCDatePicker.name, PCDatePicker);
 
